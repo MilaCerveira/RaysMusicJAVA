b9d9dd23958a48024546fd8e890610c02700ecbf
milacerveira <milascerveira@gmail.com>
2021-02-19 14:15:06 +0000

    guitar has price

---
+++ src/test/java/GuitarTest.java
@@ -0,0 +1,17 @@
+import instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class GuitarTest {
+    Guitar guitar;
+    @Before
+    public void setup(){
+        guitar = new Guitar(400.00);
+    }
+
+    @Test
+    public void hasPrice(){
+        assertEquals(400.00, guitar.getPrice(),0.1);
+    }
+}
