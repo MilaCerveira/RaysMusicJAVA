b771af9b2ab093d623502b7be47f1a4def74fe04
milacerveira <milascerveira@gmail.com>
2021-02-19 14:25:04 +0000

    violin has price

--- src/test/java/ViolinTest.java
+++ src/test/java/ViolinTest.java
@@ -1,2 +1,16 @@
+import instruments.Violin;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class ViolinTest {
+    Violin violin;
+    @Before
+    public void setUp(){
+        violin = new Violin(45000.00);
+    }
+    @Test
+    public void hasPrice(){
+        assertEquals(45000.00, violin.getPrice(), 0.1);
+    }
 }
