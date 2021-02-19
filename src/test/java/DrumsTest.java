c507f2fc07303571de6b5abb8d700fdf66a66817
milacerveira <milascerveira@gmail.com>
2021-02-19 14:07:26 +0000

    drums has price

--- src/test/java/DrumsTest.java
+++ src/test/java/DrumsTest.java
@@ -1,2 +1,20 @@
+import instruments.Drums;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DrumsTest {
+
+    Drums drums;
+
+    @Before
+    public void setUp() {
+        drums = new Drums(200.00);
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(200.00, drums.getPrice(), 0.01);
+    }
 }
